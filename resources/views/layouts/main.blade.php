a75d2e46b7936348f098a553b415363017a3d4e6
Vito Farenga <vitofarenga@libero.it>
2022-09-07 16:50:15 +0200

    struttura base completata

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC-COMICS</title>
+</head>
+<body>
+    <header>
+      @include('includes.header')
+    </header>
+
+    <main>
+      @yield('main-content')
+    </main>
+
+    <footer>
+      @include('includes.footer')
+    </footer>
+</body>
+</html>
