dbc8c64fca360e525bb5e317d04f472e761c9e93
Vito Farenga <vitofarenga@libero.it>
2022-09-07 19:53:18 +0200

    sono bloccato, non si vede nulla e non mi da errori

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = config('comics.php');
+    return view('home',['art'=>$data]);
 });
